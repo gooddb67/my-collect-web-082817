56562c44ee6fa6755a764129804cf7c4b402fe1e
David Goodman <davegoodman20@gmail.com>
2017-08-23 15:45:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)
+    i = 0
+    new_arr = [] #beacause collect returns a new array, we create the array for it to return
+    while i < array.length
+      new_arr << yield(array[i])
+      i += 1
+    end
+    new_arr
+end
